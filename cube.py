4f82bc8e1d92a0db44f55f776235a704f9a10ded
Tom Dickelman <tsdickelman@gmail.com>
2022-07-17 17:31:51 -0500

    Finds Cube Volume

---
+++ cube.py
@@ -0,0 +1,13 @@
+# Put your code here
+
+cubeSides = 6
+
+length = input("Please enter length: ")
+
+intLength = int(length)
+
+oneSide = intLength * intLength
+
+surfaceArea = oneSide * cubeSides
+
+print(f'The surface area of the cube is: {surfaceArea}.')
