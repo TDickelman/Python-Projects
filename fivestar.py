fd9ae6f020537b096ec083c494520fbe260151e9
Tom Dickelman <tsdickelman@gmail.com>
2022-07-20 11:53:38 -0500

    Movie Cost Calculator

---
+++ fivestar.py
@@ -0,0 +1,10 @@
+# Put your code here
+new = 3.0
+old = 2.0
+
+newMovie = input("Enter number of new movies: ")
+oldMovie = input("Enter number of old movies: ")
+
+total = (new * float(newMovie)) + (old * float(oldMovie))
+
+print(f'The total cost is ${total}')
