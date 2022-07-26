ea572c068e4ed29d71b5b6b11bfbb94ddf717f29
Tom Dickelman <tsdickelman@gmail.com>
2022-07-25 20:16:45 -0500

    Add files via upload

---
+++ encrypt.py
@@ -0,0 +1,8 @@
+# Put your code here
+string = input("Enter a message: ")
+n = int(input("Enter the distance value: "))
+print()
+output = ""
+for i in string:
+    output += chr(ord(i)+n)
+print(output)
