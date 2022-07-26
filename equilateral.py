7337894669fed665960d812a0651f7415460980c
Tom Dickelman <tsdickelman@gmail.com>
2022-07-25 20:14:54 -0500

    Add files via upload

---
+++ equilateral.py
@@ -0,0 +1,14 @@
+# Put your code here
+
+a = float(input("Enter first side: "))
+b = float(input("Enter second side: "))
+c = float(input("Enter third side: "))
+
+print()
+if a == b and b == c:
+    print("The triangle is equilateral.")
+else:
+    print("The triangle is not equilateral.")
+
+
+
